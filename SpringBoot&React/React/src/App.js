ba5fc2a94260291b2f5e0d8e56dc6b6227af2cb4
byeongseokim <kbs970318@gmail.com>
2023-08-01 13:05:13 +0900

    material을 이용해 UI 개선

--- SpringBoot&React/React/src/App.js
+++ SpringBoot&React/React/src/App.js
@@ -1,27 +1,34 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Todo from "./Todo";
+import { List, Paper } from "@mui/material";
 
 function App() {
   const [items, setItems] = useState([
     {
       id: "0",
       title: "Hello World 1",
-      done: true
+      done: true,
     },
     {
       id: "1",
       title: "Hello World 2",
-      done: true
+      done: true,
     },
   ]);
 
-  let todoItems =
-    items.length > 0 && items.map((item) => <Todo item={item} key={item.id} />);
-
+  let todoItems = items.length > 0 && (
+    <Paper style={{ margin: 16 }}>
+      <List>
+        {items.map((item) => (
+          <Todo item={item} key={item.id} />
+        ))}
+      </List>
+    </Paper>
+  );
   return (
     <div className='App'>
-      <Todo item={todoItems} />
+      {todoItems}
     </div>
   );
 }
--- SpringBoot&React/React/src/Todo.js
+++ SpringBoot&React/React/src/Todo.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { ListItem, ListItemText, InputBase, Checkbox } from "@mui/material";
 
-const Todo = () => {
+const Todo = (props) => {
+    const [item, setItem] = useState(props.item);
+
     return (
-        <div className="Todo">
-            <input type="checkbox" id="todo0" name="todo0" value="todo0" />
-            <label for="todo0">Todo 컴포넌트 만들기</label>
-        </div>
+        <ListItem>
+            <Checkbox checked={item.done} />
+            <ListItemText>
+                <InputBase
+                    inputProps={{ "aria-label": "naked" }}
+                    type="text"
+                    id="{item.id}"
+                    name="{item.id}"
+                    value={item.title}
+                    multiline={true}
+                    fullWidth={true}
+                />
+            </ListItemText>
+        </ListItem>
     );
 };
+
 
 export default Todo;
