69e416803805e1cca04d8437e5d04eb6c0f7c71f
byeongseokim <kbs970318@gmail.com>
2023-08-01 12:00:23 +0900

    App에서 Todo로 item 넘겨주기

--- SpringBoot&React/React/src/App.js
+++ SpringBoot&React/React/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const Todo = (props) => {
+  const [item, setItem] = useState({
+    id: "0",
+    title: "Hello World 1",
+    done: true
+  }
+  );
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Todo item={item} />
     </div>
   );
 }
 
-export default App;
+export default Todo;
---
+++ SpringBoot&React/React/src/Todo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Todo = () => {
+    return (
+        <div className="Todo">
+            <input type="checkbox" id="todo0" name="todo0" value="todo0" />
+            <label for="todo0">Todo 컴포넌트 만들기</label>
+        </div>
+    );
+};
+
+export default Todo;
