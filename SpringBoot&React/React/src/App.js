1a8a36f31ffa565cf2ec4062e97506db7340a04b
byeongseokim <kbs970318@gmail.com>
2023-08-01 12:06:00 +0900

    Todo 아이템 리스트 렌더링

--- SpringBoot&React/React/src/App.js
+++ SpringBoot&React/React/src/App.js
@@ -1,20 +1,29 @@
 import { useState } from 'react';
 import './App.css';
+import Todo from "./Todo";
 
-const Todo = (props) => {
-  const [item, setItem] = useState({
-    id: "0",
-    title: "Hello World 1",
-    done: true
-  }
-  );
+function App() {
+  const [items, setItems] = useState([
+    {
+      id: "0",
+      title: "Hello World 1",
+      done: true
+    },
+    {
+      id: "1",
+      title: "Hello World 2",
+      done: true
+    },
+  ]);
 
+  let todoItems =
+    items.length > 0 && items.map((item) => <Todo item={item} key={item.id} />);
 
   return (
     <div className='App'>
-      <Todo item={item} />
+      <Todo item={todoItems} />
     </div>
   );
 }
 
-export default Todo;
+export default App;
