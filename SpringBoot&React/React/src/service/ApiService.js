7ebbfdf8bbf72b4c68ce1a41c75e67bb842965d1
byeongseokim <kbs970318@gmail.com>
2023-08-02 11:25:04 +0900

    alert 메시지 안나오는거 해결 / 토큰 기록 확인

--- SpringBoot&React/React/src/service/ApiService.js
+++ SpringBoot&React/React/src/service/ApiService.js
@@ -1,28 +1,49 @@
 import { API_BASE_URL } from "../api-config";
 
 export function call(api, method, request) {
+    let headers = new Headers({
+        "Content-Type": "application/json",
+    });
+
+    // 로컬 스토리지에서 ACCESS TOKEN 가져오기
+    const accessToken = localStorage.getItem("ACCESS_TOKEN");
+    if (accessToken && accessToken !== null) {
+        headers.append("Authorization", "Bearer " + accessToken);
+    }
+
     let options = {
-        headers: new Headers({
-            "Content-Type": "application/json",
-        }),
+        headers: headers,
         url: API_BASE_URL + api,
         method: method,
     };
     if (request) {
-        //GET method
+        // GET method
         options.body = JSON.stringify(request);
     }
     return fetch(options.url, options).then((response) => {
         if (response.status === 200) {
             return response.json();
         } else if (response.status === 403) {
-            window.location.href = "/login"; //redirect
+            window.location.href = "/login"; // redirect
         } else {
-            Promise.reject(response);
-            throw Error(response);
+            new Error(response);
         }
     }).catch((error) => {
         console.log("http error");
         console.log(error);
     });
+}
+
+export function signin(userDTO) {
+    return call("/auth/signin", "POST", userDTO)
+        .then((response) => {
+            alert("로그인 토큰: " + response.token)
+            console.log("response :", response)
+            if (response.token) {
+                // 로컬 스토리지에 토큰 저장
+                localStorage.setItem("ACCESS_TOKEN", response.token);
+                // token이 존재하는 경우 Todo 화면으로 리디렉트
+                window.location.href = "/";
+            }
+        });
 }
