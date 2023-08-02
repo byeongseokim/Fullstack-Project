f0ca3ad2cb14547cea2539d49e62caf4a8d7e6d8
byeongseokim <kbs970318@gmail.com>
2023-08-02 11:13:05 +0900

    WebMvcConfig 클래스명 수정

--- SpringBoot&React/demo/src/main/java/com/example/demo/config/WebMvcConfig.java
+++ SpringBoot&React/demo/src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration //스프링 빈으로 등록
-public class WebMcvConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
 	
 	private final long MAX_AGE_SECS = 3600;
 
